87581fd1d5b32910f65aed05ad3f04f722f0e504
Oscar <kurtogath@gmail.com>
2024-10-21 11:39:19 +0200

    Resuelto bug no en todo el cuadrado era link

--- src/components/PlayerListData.tsx
+++ src/components/PlayerListData.tsx
@@ -8,11 +8,11 @@ export function PlayerListData({ player }: { player: Player }) {
     const flag = emojiFlags.countryCode(player.flag)?.emoji || "";
 
     return (
-        <div className="rounded-lg bg-gray-800 p-4">
-            <Link href={`/players/${player.id}`}>
+        <Link href={`/players/${player.id}`}>
+            <div className="rounded-lg bg-gray-800 p-4">
                 <h2 className="">{`${player.name}`}</h2>
                 <p>{`${flag} ${player.birthplace}`}</p>
-            </Link>
-        </div>
+            </div>
+        </Link>
     );
 }
